06b878b647d9dd57415058cd36da35dc52e1f3d7
tigvarts <tigvarts@gmail.com>
2016-06-09 13:54:56 +0300

    Added postgresql dialect for limit, added tape for explain analyse

--- tools/qgen.sh
+++ tools/qgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo query$1.tpl > tmp.txt
-./dsqgen -input tmp.txt -directory ../query_templates -dialect db2 -scale 10
+./dsqgen -input tmp.txt -directory ../query_templates -dialect postgres -scale ${1:-1}
 rm tmp.txt
 cat query_0.sql
 rm query_0.sql
