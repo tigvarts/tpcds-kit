114d4edf79d12730527dad84bfe06ef6b0316dd7
Oleg Ivanov <tigvarts@gmail.com>
2017-12-04 19:05:49 +0300

    Fixed bug

--- tools/qgen.sh
+++ tools/qgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo query$1.tpl > tmp.txt
-./dsqgen -input tmp.txt -directory ../query_templates -dialect postgres -scale ${2:-1}
+echo query${1}.tpl > tmp.txt
+./dsqgen -input tmp.txt -directory ../query_templates -dialect postgres -scale ${2:-1} -rngseed $RANDOM
 rm tmp.txt
 cat query_0.sql
 rm query_0.sql
