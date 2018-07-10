f7eeb3debb4901fbf505adeb8be70b96a56b8d09
Oleg Ivanov <tigvarts@gmail.com>
2018-07-11 00:55:42 +0300

    Improved usability.

--- tools/install.sh
+++ tools/install.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+scale=${1:-1}
+psql=${2:-psql}
+
 echo Compiling...
 cp Makefile.suite Makefile
 make
 echo
 echo Making database...
-./dsdgen -terminate N -force -dir /tmp -scale ${1:-1}
-psql -d postgres -c "drop database if exists tpcds"
-psql -d postgres -c "create database tpcds"
-psql -d tpcds < tpcds.sql
-psql -d tpcds < tpcds_load.sql
-psql -d tpcds < tpcds_ri.sql
-rm /tmp/*.dat
+mkdir -p /tmp/tpcds
+./dsdgen -terminate N -force -dir /tmp/tpcds -scale $scale
+$psql -d postgres -c "drop database if exists tpcds"
+$psql -d postgres -c "create database tpcds"
+$psql -d tpcds < tpcds.sql
+$psql -d tpcds < tpcds_load.sql
+$psql -d tpcds < tpcds_ri.sql
+rm -rf /tmp/tpcds
 echo Finished successfully.
--- tools/qgen.sh
+++ tools/qgen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo query${1}.tpl > tmp.txt
-./dsqgen -input tmp.txt -directory ../query_templates -dialect postgres -scale ${2:-1} -rngseed $RANDOM
+./dsqgen -input tmp.txt -directory ../query_templates -dialect ${2:-postgres} -scale ${3:-1} -rngseed $RANDOM
 rm tmp.txt
 cat query_0.sql
 rm query_0.sql
